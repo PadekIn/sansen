683c0328aabec91443f997db37ca01c4156a5bed
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-02-25 13:19:28 +0700

    refactor: add standar_peforma model

--- app/Models/Standar_peforma.php
+++ app/Models/Standar_peforma.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Vinkla\Hashids\Facades\Hashids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Standar_peforma extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'fcr',
+        'fi',
+        'fe',
+        'dep',
+        'abw',
+        'adg',
+        'ip'
+    ];
+
+    public function getHashidAttribute() {
+        return Hashids::encode($this->id);
+    }
 }
