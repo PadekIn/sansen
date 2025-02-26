a9772281e0695cfebf3f36d3fd0bf4a7694b8df3
vtw <vtiaraw2@gmail.com>
2025-02-26 23:50:23 +0700

    fix: deleting the wrong file hehe

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sansen Brother Farm | Beranda</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./assets/img/svg/logo.PNG" type="image/x-icon">
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div class="layer"></div>
+    <!-- ! Body -->
+    <a class="skip-link sr-only" href="#skip-target">Skip to content</a>
+    <div class="page-flex">
+        <!-- ! Sidebar -->
+        <aside class="sidebar">
+            <div class="sidebar-start">
+                <div class="sidebar-head">
+                    <a href="/" class="logo-wrapper" title="Home">
+                        <span class="sr-only">Home</span>
+                        <img src="./assets/img/svg/logo.PNG" alt="" width="59px" height="59px" />
+                        <!-- <span class="icon logo" aria-hidden="true"></span> -->
+                        <div class="logo-text">
+                            <span class="logo-title">SANSEN</span><br>
+                            <span class="logo-subtitle">BROTHER FARM</span>
+                        </div>
+
+                    </a>
+                    <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
+                        <span class="sr-only">Toggle menu</span>
+                        <span class="icon menu-toggle" aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div class="sidebar-body">
+                    <ul class="sidebar-body-menu">
+                        <li>
+                            <a class="active" href="/"><span class="icon home" aria-hidden="true"></span>Beranda</a>
+                        </li>
+                        <li>
+                            <a class="show-cat-btn" href="##">
+                                <span class="icon performa" aria-hidden="true"></span>Performa
+                                <span class="category__btn transparent-btn" title="Open list">
+                                    <span class="sr-only">Open list</span>
+                                    <span class="icon arrow-down" aria-hidden="true"></span>
+                                </span>
+                            </a>
+                            <ul class="cat-sub-menu">
+                                <li>
+                                    <a href="">Standar</a>
+                                </li>
+                                <li>
+                                    <a href="">Actual</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="show-cat-btn" href="/"><span class="icon pop" aria-hidden="true"></span>Populasi</a>
+                        </li>
+                        <li>
+                            <a class="show-cat-btn" href="/"><span class="icon activity" aria-hidden="true"></span>Perkembangan</a>
+                        </li>
+                        <li>
+                            <a class="show-cat-btn" href="/"><span class="icon wheat" aria-hidden="true"></span>Pakan</a>
+                        </li>
+                        <li>
+                            <a class="show-cat-btn" href="/"><span class="icon report" aria-hidden="true"></span>Laporan</a>
+                        </li>
+                        <li>
+                            <a href=""><span class="icon setting" aria-hidden="true"></span>Pengaturan</a>
+                        </li>
+                </div>
+            </div>
+        </aside>
+        <div class="main-wrapper">
+            <!-- ! Main nav -->
+            <nav class="main-nav--bg">
+                <div class="container main-nav">
+                    <div class="main-nav-start">
+                        <div class="search-wrapper">
+                            <i data-feather="search" aria-hidden="true"></i>
+                            <input type="text" placeholder="Enter keywords ..." required>
+                        </div>
+                    </div>
+                    <div class="main-nav-end">
+                        <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
+                            <span class="sr-only">Toggle menu</span>
+                            <span class="icon menu-toggle--gray" aria-hidden="true"></span>
+                        </button>
+                        <div class="lang-switcher-wrapper">
+                            <button class="lang-switcher transparent-btn" type="button">
+                                EN
+                                <i data-feather="chevron-down" aria-hidden="true"></i>
+                            </button>
+                            <ul class="lang-menu dropdown">
+                                <li><a href="##">English</a></li>
+                                <li><a href="##">French</a></li>
+                                <li><a href="##">Uzbek</a></li>
+                            </ul>
+                        </div>
+                        <button class="theme-switcher gray-circle-btn" type="button" title="Switch theme">
+                            <span class="sr-only">Switch theme</span>
+                            <i class="sun-icon" data-feather="sun" aria-hidden="true"></i>
+                            <i class="moon-icon" data-feather="moon" aria-hidden="true"></i>
+                        </button>
+                        <div class="notification-wrapper">
+                            <button class="gray-circle-btn dropdown-btn" title="To messages" type="button">
+                                <span class="sr-only">To messages</span>
+                                <span class="icon notification active" aria-hidden="true"></span>
+                            </button>
+                            <ul class="users-item-dropdown notification-dropdown dropdown">
+                                <li>
+                                    <a href="##">
+                                        <div class="notification-dropdown-icon info">
+                                            <i data-feather="check"></i>
+                                        </div>
+                                        <div class="notification-dropdown-text">
+                                            <span class="notification-dropdown__title">System just updated</span>
+                                            <span class="notification-dropdown__subtitle">The system has been successfully upgraded. Read more
+                                                here.</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="##">
+                                        <div class="notification-dropdown-icon danger">
+                                            <i data-feather="info" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="notification-dropdown-text">
+                                            <span class="notification-dropdown__title">The cache is full!</span>
+                                            <span class="notification-dropdown__subtitle">Unnecessary caches take up a lot of memory space and
+                                                interfere ...</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="##">
+                                        <div class="notification-dropdown-icon info">
+                                            <i data-feather="check" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="notification-dropdown-text">
+                                            <span class="notification-dropdown__title">New Subscriber here!</span>
+                                            <span class="notification-dropdown__subtitle">A new subscriber has subscribed.</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="link-to-page" href="##">Go to Notifications page</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="nav-user-wrapper">
+                            <button href="##" class="nav-user-btn dropdown-btn" title="My profile" type="button">
+                                <span class="sr-only">Profile</span>
+                                <span class="nav-user-img">
+                                    <picture>
+                                        <source srcset="./assets/img/avatar/profile-ayam.png" type="image/webp"><img src="./assets/img/avatar/profile-ayam.png" alt="User name">
+                                    </picture>
+                                </span>
+                            </button>
+                            <ul class="users-item-dropdown nav-user-dropdown dropdown">
+                                <li><a href="##">
+                                        <i data-feather="user" aria-hidden="true"></i>
+                                        <span>Profile</span>
+                                    </a></li>
+                                <li><a href="##">
+                                        <i data-feather="settings" aria-hidden="true"></i>
+                                        <span>Account settings</span>
+                                    </a></li>
+                                <form method="POST" action="{{ route(name: 'logout') }}">
+                                    @csrf
+                                    <li><a class="danger" href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                            <i data-feather="log-out" aria-hidden="true"></i>
+                                            <span>Log out</span>
+                                        </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- ! Main -->
+            <main class="main users chart-page" id="skip-target">
+                {{ $slot }}
+            </main>
+            <!-- ! Footer -->
+            <footer class="footer">
+                <div class="container footer--flex">
+                    <div class="footer-start">
+                        <p>2025 © Sansen Brother Farm <a href="" target="_blank"
+                                rel="noopener noreferrer"></a></p>
+                    </div>
+                    <ul class="footer-end">
+                        <li><a href="##">About</a></li>
+                        <li><a href="##">Support</a></li>
+                        <li><a href="##">Puchase</a></li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <!-- Chart library -->
+    <script src="./assets/plugins/chart.min.js"></script>
+    <!-- Icons library -->
+    <script src="assets/plugins/feather.min.js"></script>
+    <!-- Custom scripts -->
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
