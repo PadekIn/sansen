f9a0624b693fa2d68d7e6666475c7136f7151c1b
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-02-26 19:57:48 +0700

    perf: menambah isi DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Pakan;
+use App\Models\Performa_actual;
+use App\Models\Perkembangan;
+use App\Models\Populasi;
+use App\Models\Standar_peforma;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +24,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        Pakan::factory()->create();
+        Perkembangan::factory()->create();
+        Populasi::factory()->create();
+        Standar_peforma::factory()->create();
+        Performa_actual::factory()->create();
     }
 }
