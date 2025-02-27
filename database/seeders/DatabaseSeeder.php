f95c534e51a41b1580153b90f416b347fe79f0be
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-02-27 14:15:13 +0700

    refactor: update readme.md dan perubahan pada email databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
         ]);
         Pakan::factory()->create();
         Perkembangan::factory()->create();
