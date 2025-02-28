d4ed248ea10a302e356e7131a1ffaf0d7ef16d85
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-02-28 10:05:50 +0700

    feat: penambahan fitur penampilan standar peforma

--- app/Http/Controllers/StandarController.php
+++ app/Http/Controllers/StandarController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Standar_peforma;
 use Illuminate\Http\Request;
 
 class StandarController extends Controller
 {
-    //
+    public function index() {
+        try{
+            $standars = Standar_peforma::all();
+            return view('pages.standar.list', compact('standars'));
+        } catch (\Throwable $th) {
+            return redirect()->route('login')->with('error', 'Something went wrong');
+        }
+    }
 }
