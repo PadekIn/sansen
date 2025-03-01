75d164da6cdf48ec5979e533ce6d16b2b7d3c205
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-03-01 20:16:35 +0700

    refactor: memperbagus table

--- resources/views/pages/performa/standar/list.blade.php
+++ resources/views/pages/performa/standar/list.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-<div class="pagetitle">
+<div class="pagetitle px-4">
     <h1>Standar Performa</h1>
     <nav>
         <ol class="breadcrumb">
@@ -9,62 +9,58 @@
     </nav>
 </div><!-- End Page Title -->
 
-<section class="section">
-    <div class="row">
-        <div class="col-lg-12">
-
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <h5 class="card-title">
-                            {{-- <a href="{{ route('') }}" class="btn btn-primary">Daftarkan Standar Performa
-                                Baru</a> --}}
-                        </h5>
-                    </div>
-                    <!-- Table with stripped rows -->
-                    <table id="listTable" class="table datatable">
-                        <thead>
-                            <tr style="font-weight: bold">
-                                <td>fcr</td>
-                                <td>fi</td>
-                                <td>fe</td>
-                                <td>dep</td>
-                                <td>abw</td>
-                                <td>adg</td>
-                                <td>ip</td>
-                                <td class="no-print">Action</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($standars as $standar)
-                                <tr>
-                                    <td>{{ $standar->fcr }}</td>
-                                    <td>{{ $standar->fi }}</td>
-                                    <td>{{ $standar->fe }}</td>
-                                    <td>{{ $standar->dep }}</td>
-                                    <td>{{ $standar->abw }}</td>
-                                    <td>{{ $standar->adg }}</td>
-                                    <td>{{ $standar->ip }}</td>
-                                    <td class="no-print">
-                                        {{-- <div class="d-flex">
-                                            <a href="{{ route('', $aset->hashid) }}"
-                                                class="btn btn-sm btn-warning">Edit</a>
-                                            <div style="width: 10px;"></div>
-                                            <button type="button" onclick="destroy('{{ $aset->hashid }}')"
-                                                class="btn btn-sm btn-danger">Delete</button>
-                                        </div> --}}
-                                    </td>
-                                </tr>
-                            @endforeach
-
-                        </tbody>
-                    </table>
-                    <!-- End Table with stripped rows -->
-                </div>
+<section class="section p-4">
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center">
+                <h5 class="card-title">
+                    Lorem ipsum dolor sit amet.
+                    {{-- <a href="{{ route('') }}" class="btn btn-primary">Tambah Standar Performa
+                        Baru</a> --}}
+                </h5>
             </div>
+            <!-- Table with stripped rows -->
+            <table id="listTable" class="table datatable">
+                <thead>
+                    <tr style="font-weight: bold">
+                        <td>fcr</td>
+                        <td>fi</td>
+                        <td>fe</td>
+                        <td>dep</td>
+                        <td>abw</td>
+                        <td>adg</td>
+                        <td>ip</td>
+                        <td class="no-print">Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($standars as $standar)
+                        <tr>
+                            <td>{{ $standar->fcr }}</td>
+                            <td>{{ $standar->fi }}</td>
+                            <td>{{ $standar->fe }}</td>
+                            <td>{{ $standar->dep }}</td>
+                            <td>{{ $standar->abw }}</td>
+                            <td>{{ $standar->adg }}</td>
+                            <td>{{ $standar->ip }}</td>
+                            <td class="no-print">
+                                {{-- <div class="d-flex">
+                                    <a href="{{ route('', $aset->hashid) }}"
+                                        class="btn btn-sm btn-warning">Edit</a>
+                                    <div style="width: 10px;"></div>
+                                    <button type="button" onclick="destroy('{{ $aset->hashid }}')"
+                                        class="btn btn-sm btn-danger">Delete</button>
+                                </div> --}}
+                            </td>
+                        </tr>
+                    @endforeach
 
+                </tbody>
+            </table>
+            <!-- End Table with stripped rows -->
         </div>
     </div>
+
 </section>
 
 <script>
