2f9b4d358309fb08bfd2a085d1bffc1780f2c0f3
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-03-03 13:23:52 +0700

    feat: menambah fungsi create di perkembangan

--- app/Http/Controllers/PerkembanganController.php
+++ app/Http/Controllers/PerkembanganController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pakan;
 use App\Models\Perkembangan;
+use App\Models\Populasi;
 use Illuminate\Http\Request;
 
 class PerkembanganController extends Controller
@@ -10,6 +12,16 @@ class PerkembanganController extends Controller
     public function index() {
         try {
             $perkembangans = Perkembangan::with('populasi', 'pakanAtas', 'pakanBawah')->get();
+        } catch (\Throwable $th) {
+            return redirect()->route('login')->with('error', 'Something went wrong');
+        }
+    }
+
+    public function create() {
+        try{
+            $populasis = Populasi::all();
+            $pakans = Pakan::all();
+            return view('pages.perkembangan.create', compact('populasis', 'pakans'));
         } catch (\Throwable $th) {
             return redirect()->route('login')->with('error', 'Something went wrong');
         }
