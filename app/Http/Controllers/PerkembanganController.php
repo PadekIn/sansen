a4f0c926edfa82eed347c592ba4b255e33074f15
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-03-03 13:20:55 +0700

    feat: penambahan controller perkembangan

--- app/Http/Controllers/PerkembanganController.php
+++ app/Http/Controllers/PerkembanganController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Perkembangan;
 use Illuminate\Http\Request;
 
 class PerkembanganController extends Controller
 {
-    //
+    public function index() {
+        try {
+            $perkembangans = Perkembangan::with('populasi', 'pakanAtas', 'pakanBawah')->get();
+        } catch (\Throwable $th) {
+            return redirect()->route('login')->with('error', 'Something went wrong');
+        }
+    }
 }
