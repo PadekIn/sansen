636512789ef306c807af85838f4f1522c78fb3ee
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-03-03 13:44:02 +0700

    feat: penambahan fungsi store

--- app/Http/Controllers/PerkembanganController.php
+++ app/Http/Controllers/PerkembanganController.php
@@ -6,6 +6,7 @@ use App\Models\Pakan;
 use App\Models\Perkembangan;
 use App\Models\Populasi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PerkembanganController extends Controller
 {
@@ -25,6 +26,32 @@ class PerkembanganController extends Controller
             return view('pages.perkembangan.create', compact('populasis', 'pakans'));
         } catch (\Throwable $th) {
             return redirect()->route('login')->with('error', 'Something went wrong');
+        }
+    }
+
+    public function store(Request $request) {
+        try {
+            $validator = Validator::make($request->all(), [
+                'populasi_id' => 'required|exists:populasis,id',
+                'umur' => 'required|integer',
+                'kematian_atas' => 'required|integer',
+                'kematian_bawah' => 'required|integer',
+                'id_tipe_pakan_atas' => 'nullable|exists:pakans,id',
+                'pakan_atas' => 'required|integer',
+                'id_tipe_pakan_bawah' => 'nullable|exists:pakans,id',
+                'pakan_bawah' => 'required|integer',
+                'abw_normal_atas' => 'required|numeric',
+                'abw_normal_bawah' => 'required|numeric',
+            ]);
+
+            if ($validator->fails()) {
+                return back()->withErrors($validator)->withInput();
+            }
+
+            Perkembangan::create($request->all());
+            return redirect()->route('main.perkembangan')->with('success', 'Perkembangan created successfully');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
         }
     }
 }
