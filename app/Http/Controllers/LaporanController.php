e0a3a2258e9ee20dda3e062ea23e8aeceb12f197
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-03-06 10:14:45 +0700

    feat: menambah function index di laporan controller

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Standar_peforma;
+use App\Models\performa_actual;
+use App\Models\Populasi;
+use App\Models\Pakan;
+use App\Models\Perkembangan;
 
 class LaporanController extends Controller
 {
-    //
+    public function index() {
+        try {
+            $standars = Standar_peforma::all();
+            $actuals = Performa_actual::with('populasi')->get();
+            $populasis = Populasi::all();
+            $pakans = Pakan::all();
+            $perkembangans = Perkembangan::with('populasi', 'pakanAtas', 'pakanBawah')->get();
+            return view('pages.laporan.index', compact('standars', 'actuals', 'populasis', 'pakans', 'perkembangans'));
+        } catch (\Throwable $th) {
+            return redirect()->route('login')->with('error', 'Something went wrong');
+        }
+    }
 }
