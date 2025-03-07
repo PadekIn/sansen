76e4264ce18f3d1337f74eca9dac1478620ebe74
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-03-07 18:48:25 +0700

    refator: sementara ini dulu

--- resources/views/pages/laporan/laporan.blade.php
+++ resources/views/pages/laporan/laporan.blade.php
@@ -1,3 +1,77 @@
 <x-app-layout>
     <h1>laporan</h1>
+    <table id="listTable" class="table datatable">
+        <thead>
+            <tr style="font-weight: bold">
+                <td>PERF</td>
+                <td>FCR</td>
+                <td>FI</td>
+                <td>FE</td>
+                <td>DEP</td>
+                <td>ABW</td>
+                <td>ADG</td>
+                <td>IP</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($standars as $standar)
+                <tr>
+                    <td>Actual</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endforeach
+
+        </tbody>
+        <tbody>
+            @foreach ($standars as $standar)
+                <tr>
+                    <td>Standar</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endforeach
+
+        </tbody>
+        <tbody>
+            @foreach ($standars as $standar)
+                <tr>
+                    <td>DIFFT</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endforeach
+
+        </tbody>
+        <tbody>
+            @foreach ($standars as $standar)
+                <tr>
+                    <td>Persentase</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
 </x-app-layout>
